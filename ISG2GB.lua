a76501e512922c712dc6753d3e68e8bfeaf69d56
ARBIX <youssefalsadick@gmail.com>
2025-01-15 20:22:35 +0300

    Create ISG2GB.lua

---
+++ ISG2GB.lua
@@ -0,0 +1,12 @@
+-- file is open source if you want it give credit and if you steal it you are gay :)
+local game.name = "Impossible Squid game 2 bridge"
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+
+local Window = Library:NewWindow("Arbix Hub |" game.name)
+
+local Section = Window:NewSection("Segment")
+
+Section:CreateButton("Show Wrong Segment", function()
+    
+        end)
